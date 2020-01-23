b95de3ec9ad24ee0b84d4844e544d4413a6c3da3
Jesse Edwards <jesse.david.edwards.1987@gmail.com>
2020-01-23 17:15:34 -0600

    ball and paddles added

--- src/components/Paddle.js
+++ src/components/Paddle.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Paddle.css";
-
 export default function Paddle() {
-  return <div className="paddle" />;
+  const [paddleX, setPaddleX] = useState(0);
+  function handleMouse(e) {
+    console.log(e.x);
+    let boundedX;
+    const offset = (window.innerWidth - 500)/2;
+    if (e.x - offset < 0) {
+      boundedX = 0;
+    } else if (e.x - offset > 400) {
+      boundedX = 400;
+    } else {
+      boundedX = e.x - offset;
+    }
+    setPaddleX(boundedX);
+  }
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouse)
+  }, []);
+  return <div className="paddle" 
+  style={{
+    left: `${paddleX}px`
+  }}
+  />;
 }
